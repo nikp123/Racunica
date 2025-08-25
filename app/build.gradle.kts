f4e27194853638edd08cc01735d2437ffa457ca9
nikp123 <nikp123@e.email>
2025-08-25 21:38:01 +0200

    Fix release (for real this time guys I swear :P)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,52 +6,54 @@ plugins {
 }
 
 android {
-    namespace = "com.github.nikp123.racunica"
-    compileSdk = 36
+  namespace = "com.github.nikp123.racunica"
+  compileSdk = 36
 
-    defaultConfig {
-        applicationId = "com.github.nikp123.racunica"
-        minSdk = 31
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+  defaultConfig {
+      applicationId = "com.github.nikp123.racunica"
+      minSdk = 31
+      targetSdk = 36
+      versionCode = 1
+      versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+      testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+  
+  signingConfigs {
+    create("release") {
+      storeFile = file("../private/keys/github-release.jks")
+      storePassword = System.getenv("STORE_PASSWORD") ?: "missing-store-password"
+      keyAlias = System.getenv("KEY_ALIAS") ?: "missing-key-alias"
+      keyPassword = System.getenv("KEY_PASSWORD") ?: "missing-key-password"
     }
+  }
 
-    buildTypes {
-      release {
-          isMinifyEnabled = false
-          proguardFiles(
-              getDefaultProguardFile("proguard-android-optimize.txt"),
-              "proguard-rules.pro"
-          )
+  buildTypes {
+    release {
+        signingConfig = signingConfigs.getByName("release")
+        isMinifyEnabled = false
+        proguardFiles(
+            getDefaultProguardFile("proguard-android-optimize.txt"),
+            "proguard-rules.pro"
+        )
+    }
+  }
+  compileOptions {
+      sourceCompatibility = JavaVersion.VERSION_11
+      targetCompatibility = JavaVersion.VERSION_11
+  }
+  kotlinOptions {
+      jvmTarget = "11"
+  }
+  buildFeatures {
+      viewBinding = true
+  }
+  packaging {
+      resources {
+          excludes += "META-INF/DEPENDENCIES"
+          excludes += "mozilla/public-suffix-list.txt"
       }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        viewBinding = true
-    }
-    packaging {
-        resources {
-            excludes += "META-INF/DEPENDENCIES"
-            excludes += "mozilla/public-suffix-list.txt"
-        }
-    }
-    signingConfigs {
-      create("release") {
-        storeFile = file("../private/keys/github-release.jks")
-        storePassword = System.getenv("STORE_PASSWORD") ?: "missing-store-password"
-        keyAlias = System.getenv("KEY_ALIAS") ?: "missing-key-alias"
-        keyPassword = System.getenv("KEY_PASSWORD") ?: "missing-key-password"
-      }
-    }
+  }
 }
 
 dependencies {
