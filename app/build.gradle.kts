97e0e4322362d83de308f73098d69d0906b63c42
nikp123 <nikp123@e.email>
2025-08-25 22:21:19 +0200

    She runs at SDK 21 with literally 0 effort, amazing.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
   defaultConfig {
       applicationId = "com.github.nikp123.racunica"
-      minSdk = 31
+      minSdk = 21
       targetSdk = 36
       versionCode = 1
       versionName = "1.0"
