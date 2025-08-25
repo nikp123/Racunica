a808938729373417e1812880bc9196204bbac310
nikp123 <nikp123@e.email>
2025-08-25 20:48:41 +0200

    Fix signing not happening?

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,13 +20,13 @@ android {
     }
 
     buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+      release {
+          isMinifyEnabled = true
+          proguardFiles(
+              getDefaultProguardFile("proguard-android-optimize.txt"),
+              "proguard-rules.pro"
+          )
+      }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
